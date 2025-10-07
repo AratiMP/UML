6b25c6105a011060acb36d4ab6ecfa36ff7ef442
Ari <a.puoommangalathu@campus.fct.unl.pt>
2025-10-07 17:31:14 +0300

    Flight Abstract Class, IndirectFlight and Direct Flight classes skeleton.

---
+++ src/Flight.java
@@ -0,0 +1,53 @@
+import java.time.LocalDateTime;
+import java.util.*;
+
+public abstract class Flight {
+    private String flightNumber;
+    private LocalDateTime departueHour;
+    private String boardingGate;
+    private Airport destination;
+    private Airline airline;
+    private Airplane airplane;
+    private ArrayList<Passenger> passengers;
+
+    public Flight(String flightNumber, LocalDateTime departueHour,
+                        String boardingGate, Airport destination, Airline airline, Airplane airplane){
+        this.flightNumber = flightNumber;
+        this.departueHour = departueHour;
+        this.boardingGate = boardingGate;
+        this.destination = destination;
+        this.airline = airline;
+        this.airplane = airplane;
+        this.passengers = new ArrayList<Passenger>();
+    }
+
+    public abstract double getFlightCost();
+
+    public String getFlightNumber(){
+        return this.flightNumber;
+    }
+
+    public LocalDateTime getDepartureHour() {
+        return departueHour;
+    }
+
+    public String getBoardingGate(){
+        return this.boardingGate;
+    }
+
+    public String getAirline(){
+        return this.airline.getName();
+    }
+
+    public String getAirplane(){
+        return this.airplane.getName();
+    }
+
+    public String getDestination(){
+        return this.destination.getName();
+    }
+
+    public Iterator<Passenger> getPassengers(){
+        return passengers.iterator();
+    }
+}
